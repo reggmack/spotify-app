f36f89380826fd2469f6e6ef77a6a8afd5bfc1cc
reggmack <thegraphixninja@gmail.com>
2017-03-14 16:58:18 -0500

    Search Component & Service
    Search Component & Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from './services/spotify.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  providers: [SpotifyService]
 })
 export class AppComponent {
   title = 'Spotify app works!';
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'about',
-  templateUrl: 'about.component.html',
+    moduleId:module.id,
+    selector: 'about',
+    templateUrl: 'about.component.html'
 })
-export class AboutComponent {
-}
+export class AboutComponent { }
