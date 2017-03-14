83a53f0f69345fd16305e02322103ce76ad40426
reggmack <thegraphixninja@gmail.com>
2017-03-14 12:23:06 -0500

    Navbar Set Up
    Navbar Set Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SearchComponent, NavbarComponent, AboutComponent
   ],
   imports: [
     BrowserModule,
